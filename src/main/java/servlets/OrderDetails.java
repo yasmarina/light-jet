5ce468ac30499ed4ad65e1c030f3ef7914a89266
Eugene Dawn <eugene.i.dawn@gmail.com>
2016-06-17 17:58:02 +0300

    trace messages removed, log messages added

--- src/main/java/servlets/OrderDetails.java
+++ src/main/java/servlets/OrderDetails.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import dao.interfaces.FlightDao;
+import lombok.extern.log4j.Log4j;
 import model.Flight;
 
 import javax.servlet.ServletConfig;
@@ -15,26 +16,18 @@ import static listeners.DbInit.FLIGHT_DAO;
 import static model.Ticket.LUGGAGE_PRICE;
 import static model.Ticket.PRIORITY_PRICE;
 
-
+@Log4j
 @WebServlet({"/orderDetails"})
 public class OrderDetails extends HttpServlet {
     private FlightDao flightDao;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        System.out.println("OrderDetails: init()");
         flightDao = (FlightDao) config.getServletContext().getAttribute(FLIGHT_DAO);
     }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("OrderDetails: doGet()");
-        super.doGet(request, response);
-    }
-
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("OrderDetails: doPost()");
         boolean luggage = Boolean.parseBoolean(request.getParameter("luggage"));
         boolean priority = Boolean.parseBoolean(request.getParameter("priority"));
         Flight flight = (Flight) (request.getSession(true).getAttribute("flight"));
@@ -44,7 +37,7 @@ public class OrderDetails extends HttpServlet {
         request.getSession(true).setAttribute("luggage", luggage);
         request.getSession(true).setAttribute("priority", priority);
         request.getSession(true).setAttribute("price", price);
-        System.out.println("ticket options (luggage, priority) and ticket price added to session!");
+        log.info("ticket options (luggage, priority) and ticket price added to session!");
         request.getRequestDispatcher("/order/confirm/index.jsp").forward(request, response);
     }
 }
